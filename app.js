9ca315310256fcfcb8363ede8c500f5db2d81530
chamer79 <chamer79@gmail.com>
2024-07-23 20:36:45 -0400

    added vaiables, cache elements, & event listener

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 /*-------------------------------- Constants --------------------------------*/
 
+
+
 /*-------------------------------- Variables --------------------------------*/
+let total = ""
+let num1 = ""
+let num2 = ""
+let operator = ""
+let reset = "0"
+
+// console.log(`test: ${num1}`)
+
 
 /*------------------------ Cached Element References ------------------------*/
+const calculator = document.querySelector("#calculator")    //<- parent node/element
+console.log(calculator)
+const displayEl = document.querySelector(".display")
+
+
 
 /*----------------------------- Event Listeners -----------------------------*/
+calculator.addEventListener("click", (event) => {
+    // to test/verify if getting the correct value when button is clicked -> 8 button prints 8
+    console.log(event.target.innerText)
+
+
+})
+
 
 /*-------------------------------- Functions --------------------------------*/
+
+  
