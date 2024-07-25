d1b1b6a2a52d82dbe485be190c06aae5a3846961
chamer79 <chamer79@gmail.com>
2024-07-24 20:20:05 -0400

    got numbers & operators to display

--- app.js
+++ app.js
@@ -3,29 +3,41 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-let total = ""
-let num1 = ""
-let num2 = ""
-let operator = ""
-let reset = "0"
+let total 
+let num1 = 0   //<- if doesn't work, try null
+let num2 = 0
+let operator    // <- no equal sign - initializing
+let reset = 0
 
 // console.log(`test: ${num1}`)
 
 
 /*------------------------ Cached Element References ------------------------*/
-const calculator = document.querySelector("#calculator")    //<- parent node/element
-console.log(calculator)
-const displayEl = document.querySelector(".display")
 
+const displayEl = document.querySelector(".display")
+const numEl = document.querySelectorAll(".number")
+const operatorEl = document.querySelectorAll(".operator")
+const equalEl = document.querySelector(".equals")
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-calculator.addEventListener("click", (event) => {
-    // to test/verify if getting the correct value when button is clicked -> 8 button prints 8
-    console.log(event.target.innerText)
+numEl.forEach((num) => {
+    num.addEventListener("click", (event) => {
+        let currentNum = event.target.textContent
 
-
+        displayEl.textContent = currentNum
+    })
 })
+
+operatorEl.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        let currentOperator = event.target.textContent
+
+        displayEl.textContent = currentOperator
+    })
+})
+
+
 
 
 /*-------------------------------- Functions --------------------------------*/
